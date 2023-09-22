9b1a2729de2a5703939c733c4a73c8d4fb98d015
RobertBenyamin <robertbenyamino@gmail.com>
2023-09-22 22:44:32 +0700

    feat: implements register, login, and logout user

--- main/models.py
+++ main/models.py
@@ -1,6 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class User(AbstractUser):
+    name = models.CharField(max_length=255)
+    email = models.EmailField(unique=True)
+    warehouse_location = models.TextField(unique=True)
 
 class Item(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=255)
     amount = models.IntegerField()
     description = models.TextField()
--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 from main.views import home, create_item, show_xml, show_json, show_xml_by_id, show_json_by_id 
+from main.views import register, login_user, logout_user
 
 app_name = 'main'
 
@@ -8,6 +9,10 @@ urlpatterns = [
     path('', views.home, name='home'),
 
     path('create-item', create_item, name='create_item'),
+
+    path('register/', register, name='register'),
+    path('login/', login_user, name='login'),
+    path('logout/', logout_user, name='logout'),
 
     path('xml/', show_xml, name='show_xml'), 
     path('xml/<int:id>/', show_xml_by_id, name='show_xml_by_id'),
